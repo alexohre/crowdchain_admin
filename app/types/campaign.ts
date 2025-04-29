d5e2a8340811cd0a836d3b623e06ee4aaa1792ff
Awointa <akosawointa@gmail.com>
2025-04-29 13:52:18 +0100

    feat: create a reusable campaign file to use across pages

---
+++ app/types/campaign.ts
@@ -0,0 +1,10 @@
+export interface Campaign {
+    id: string
+    name: string
+    creatorAddress: string
+    category: string
+    raised: number
+    goal: number
+    status: string
+    timeline: string
+  }
